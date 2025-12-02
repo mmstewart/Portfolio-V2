f06ad5f970fb66e7796e567af5357536b36fd3b0
mmstewart <mmsstewart20@gmail.com>
2025-12-01 18:49:43 -0600

    added skeleton placeholder to images

--- src/components/card.js
+++ src/components/card.js
@@ -1,13 +1,26 @@
 import * as React from 'react';
 
 export default function Card({ src, alt, title, description, badges = [], buttons = [] }) {
+	const [loading, setLoading] = React.useState(true);
+
+	React.useEffect(() => {
+		setTimeout(() => {
+			setLoading(false);
+		}, 1000);
+	}, []);
+
 	return (
 		<div className="card bg-base-100 w-96 border border-base-300">
 			<figure>
-				<img
-					src={src}
-					alt={alt}
-				/>
+				{loading ? (
+					<div className="h-[199px] w-[382px] skeleton rounded-none" />
+				) : (
+					<img
+						src={src}
+						alt={alt}
+						onLoad={() => setLoading(false)}
+					/>
+				)}
 			</figure>
 			<div className="card-body">
 				<h2 className="card-title">{title}</h2>
